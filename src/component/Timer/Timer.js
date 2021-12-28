22f5bf5b371b3904e1aa6cd67fd3ee09d23decca
shangjuc <shangjuc@gmail.com>
2021-12-28 15:06:46 +0800

    Add date-fns-tz

--- src/component/Timer/Timer.js
+++ src/component/Timer/Timer.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Timer.scss';
+import { formatInTimeZone } from 'date-fns-tz'
+
+// const date = new Date('2014-10-25T10:46:20Z')
+const date = new Date()
+console.log(formatInTimeZone(date, 'America/New_York', 'yyyy-MM-dd HH:mm:ss XXX'))
+console.log(formatInTimeZone(date, 'Asia/Taipei', 'yyyy-MM-dd HH:mm:ss XXX'))
 
 
 class Clock2 extends React.Component {
@@ -29,7 +35,9 @@ class Clock2 extends React.Component {
         return (
             <div className="container">
                 <h1>Hello, {this.props.user}</h1>
-                <h2>{this.state.date.toLocaleTimeString()}</h2>
+                {/* <h2>{this.state.date.toLocaleTimeString()}</h2> */}
+                <p>NY: {formatInTimeZone(this.state.date, 'America/New_York', 'yyyy-MM-dd HH:mm:ss XXX')}</p>
+                <p>TW: {formatInTimeZone(this.state.date, 'Asia/Taipei', 'yyyy-MM-dd HH:mm:ss XXX')}</p>
             </div>
         )
     }
@@ -43,6 +51,7 @@ class Clock2 extends React.Component {
 //         </div>
 //     )
 // }
+
 
 function tick() {
     if (!document.getElementById('timer')){
