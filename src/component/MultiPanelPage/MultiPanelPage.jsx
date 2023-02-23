f28858c656d47dbfec0941ae987b4996b73bd771
SJ Chen <shangjuc@gmail.com>
2023-02-23 15:29:47 +0800

    Add MPP

---
+++ src/component/MultiPanelPage/MultiPanelPage.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from './logo.svg';
+
+function MultiPanelPage(props) {
+    const [panal_list, setPanelList] = useState('');
+}
+
+export default MultiPanelPage;
