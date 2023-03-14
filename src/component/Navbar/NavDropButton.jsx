471b8b9dc1d33e712181ef70627d69529b8bd4b1
shangjuc <shangjuc@gmail.com>
2023-03-14 17:40:49 +0800

    Update navbar

--- src/component/Navbar/NavDropButton.jsx
+++ src/component/Navbar/NavDropButton.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.scss';
 
 
@@ -8,23 +8,34 @@ function NavDropButton(props) {
     const list_items = numbers.map((num, index) =>
         <li className="list-item" key={index} my_key={props.nav_value + num} onClick={click_btn}>{props.nav_value}{num}</li>
     )
+    const [showList, setShowList] = useState(false)
     let show_list = false;
+    
     function click_btn(e) {
         console.log(e.target.getAttribute('my_key'))
+        // setShowList(true)
+    }
+    function click_drop_btn(){
+        // console.log(showList)
+        setShowList(!showList)
+        // console.log(props.nav_value)
     }
     if (props.nav_index === props.nav_value) {
         show_list = true;
+        // setShowList(true)
     } else {
         show_list = false;
+        // setShowList(false)
     }
 
     return (
-        <li className={'nav-btn unselectable ' + (show_list ? "active" : "")}
-            onClick={props.click_navbtn}
+        <li className={'nav-btn unselectable ' + (showList ? "active" : "")}
+            // onClick={props.click_navbtn}
+            onClick={click_drop_btn}
             nav_value={props.nav_value}
             nav_index={props.nav_index}
         ><span nav_value={props.nav_value}>{props.nav_value}</span>
-            <div className={'droplist '} hidden={show_list ? false : true}>
+            <div className={'droplist '} hidden={showList ? false : true}>
                 <ul>
                     {list_items}
                 </ul>
